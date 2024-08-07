8a3fca22cf931f40000c14a306a1cf2240943442
Luboslav <luboz3ro@gmail.com>
2024-08-07 13:47:14 +0300

    1.45 video commit

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const page = () => {
+const SignUp = () => {
   return (
     <div>SignUp</div>
   )
 }
 
-export default page
+export default SignUp
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,38 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = {firstName: 'Lubo', lastName: 'stf', email: '@lubostf'};
+
+
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+           type="greeting"
+           title="Welcome"
+           user={loggedIn?.firstName || 'Guest'}
+           subtext="Access and manage your accounts and assets"
+          />
+
+          <TotalBalanceBox
+           accounts={[]}
+           totalBanks={1}
+           totalCurrentBalance={1250.35}
+           />
+        </header>
+
+      </div>
+
+      <RightSidebar
+       user={loggedIn}
+       transactions={[]}
+       banks={[{}, {}]} 
+       />
+    </section>
   )
 }
 
